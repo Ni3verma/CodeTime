e81067c8d565072435dddf3d1a84fdf636fd494c
Ni3Verma <canvas.nv@gmail.com>
2018-05-24 23:11:23 +0530

    initial commit

---
+++ app/src/androidTest/java/com/Import/codetime/ExampleInstrumentedTest.java
@@ -0,0 +1,13 @@
+<application>
+<component name="UsageTrigger">
+<counts>
+<entry feature="StripeButton[Android Profiler].hidden"count="2"/>
+<entry feature="StripeButton[Android Profiler].shown"count="1"/>
+<entry feature="StripeButton[Android_Profiler].hidden"count="138"/>
+<entry feature="StripeButton[Android_Profiler].shown"count="87"/>
+<entry feature="StripeButton[Version Control].shown"count="1"/>
+<entry feature="StripeButton[Version_Control].shown"count="3"/>
+<entry feature="execute.AndroidJUnit.Run"count="1"/>
+<entry feature="execute.AndroidRunConfigurationType.Debug"count="71"/>
+<entry feature="execute.AndroidRunConfigurationType.Run"count="474"/>
+  
---
+++ app/src/test/java/com/Import/codetime/ExampleUnitTest.java
@@ -0,0 +1,10 @@
+<?xml version="1.0"encoding="UTF-8"?>
+<project version="4">
+<component name="AndroidLayouts">
+<shared>
+<config />
+</shared>
+</component>
+<component name="ChangeListManager">
+<list default="true"id="933d523d-16fe-4666-88ae-bfeefc9f46ca"name="Default"comment="">
+<change beforePath="$PROJECT_DIR$/.idea/misc.xml"afterPath="$PROJECT_DIR$/.idea/misc
