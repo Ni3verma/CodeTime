a793284b46b561f9f92f44b058e3c4598ccc7d84
Nitin <nitin@Ubuntu-16.04>
2018-05-26 15:24:10 +0530

    complete main activity layout

--- app/src/main/java/com/Import/codetime/MainActivity.java
+++ app/src/main/java/com/Import/codetime/MainActivity.java
@@ -1,7 +1,9 @@
 package com.Import.codetime;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting gradient animation for text views of each card
+        TextView tv1 = findViewById(R.id.past_events_tv);
+        TextView tv2 = findViewById(R.id.ongoing_events_tv);
+        TextView tv3 = findViewById(R.id.future_events_tv);
+        setTextViewAnimation(tv1);
+        setTextViewAnimation(tv2);
+        setTextViewAnimation(tv3);
+
+    }
+
+    void setTextViewAnimation(TextView tv){
+        AnimationDrawable animationDrawable = (AnimationDrawable) tv.getBackground();
+        animationDrawable.setEnterFadeDuration(0);
+        animationDrawable.setExitFadeDuration(1000);
+        animationDrawable.start();
     }
 }
