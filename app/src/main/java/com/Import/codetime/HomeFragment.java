71fa92817c98006477c8f4b45ef1fa9215d49869
Ni3Verma <canvas.nv@gmail.com>
2018-11-03 15:45:22 +0530

    minor fixes

--- app/src/main/java/com/Import/codetime/HomeFragment.java
+++ app/src/main/java/com/Import/codetime/HomeFragment.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
@@ -58,7 +56,7 @@ public class HomeFragment extends Fragment {
     void setTextViewAnimation(TextView tv){
         AnimationDrawable animationDrawable = (AnimationDrawable) tv.getBackground();
         animationDrawable.setEnterFadeDuration(0);
-        animationDrawable.setExitFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(800);
         animationDrawable.start();
     }
 }
