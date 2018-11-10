39bf2290e7bb63949169085d6e5d5b11713eb893
Nitin <canvas.nv@gmail.com>
2018-11-11 01:37:54 +0530

    share and open browser fab buttons working

--- app/src/main/java/com/Import/codetime/ContestDetailActivity.java
+++ app/src/main/java/com/Import/codetime/ContestDetailActivity.java
@@ -1,6 +1,14 @@
 package com.Import.codetime;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -8,6 +16,9 @@ import android.widget.TextView;
 import com.Import.codetime.database.AppDatabase;
 import com.Import.codetime.database.ContestEntry;
 import com.Import.codetime.database.MyDiskExecutor;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class ContestDetailActivity extends AppCompatActivity {
     public static final String ID_EXTRA_KEY = "idKey";
@@ -42,12 +53,47 @@ public class ContestDetailActivity extends AppCompatActivity {
     }
 
     public void addToCalender(View view) {
+        //bug: not working
+        Calendar startTime = Calendar.getInstance();
+        startTime.set(2018, 11, 12, 1, 19);
+        long startTimeMillis = startTime.getTimeInMillis();
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2018, 11, 13, 1, 20);
+        long endTimeMillis = endTime.getTimeInMillis();
+
+        ContentResolver contentResolver = getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(CalendarContract.Events.DTSTART, startTimeMillis);
+        values.put(CalendarContract.Events.DTEND, endTimeMillis);
+        values.put(CalendarContract.Events.TITLE, "time to CODE");
+        values.put(CalendarContract.Events.DESCRIPTION, "Check out this contest:\n" + contest.getName() + "\nClick on this link and register for it: " + contest.getURL());
+        values.put(CalendarContract.Events.CALENDAR_ID, 3);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getDisplayName());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Uri uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values);
 
     }
 
     public void shareContest(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Time to CODE !!");
+        intent.putExtra(Intent.EXTRA_TEXT, "Check out this contest:\n" + contest.getName() + "\nClick on this link and register for it: " + contest.getURL());
+        startActivity(Intent.createChooser(intent, "sharing is caring"));
     }
 
     public void openInBrowser(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(contest.getURL()));
+        startActivity(intent);
     }
 }
