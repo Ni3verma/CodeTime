1cd9fb49e2a41aeb67351ec22dfd213e4df57de5
Nitin <canvas.nv@gmail.com>
2018-11-13 00:07:06 +0530

    preference screen made

--- app/src/main/java/com/Import/codetime/SettingsActivity.java
+++ app/src/main/java/com/Import/codetime/SettingsActivity.java
@@ -17,6 +17,7 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
     }
 
     @Override
--- app/src/main/java/com/Import/codetime/SettingsFragment.java
+++ app/src/main/java/com/Import/codetime/SettingsFragment.java
@@ -7,5 +7,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_resources);
+
+
     }
 }
