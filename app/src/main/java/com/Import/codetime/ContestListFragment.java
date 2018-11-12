7c0b26ffc94f841798ecc1da4c03eee5b347a8e1
Nitin <canvas.nv@gmail.com>
2018-11-13 00:36:33 +0530

    get resource regex method fixed

--- app/src/main/java/com/Import/codetime/ContestListFragment.java
+++ app/src/main/java/com/Import/codetime/ContestListFragment.java
@@ -3,6 +3,7 @@ package com.Import.codetime;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -14,6 +15,7 @@ import android.renderscript.ScriptIntrinsicBlur;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.transition.TransitionInflater;
@@ -39,6 +41,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Set;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -273,8 +276,18 @@ public class ContestListFragment extends Fragment {
     };
 
     private String getFavouriteResourcesRegex() {
-        //optimize:this is TEMPORARY, once resources activity is made,return properly
-        return "hackerrank.com|codechef.com|codeforces.com|topcoder.com|hackerearth.com";
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        Set<String> keySet = sharedPreferences.getAll().keySet();
+        StringBuilder resourceRegex = new StringBuilder();
+        for (String key : keySet) {
+            if (sharedPreferences.getBoolean(key, true)) {
+                resourceRegex.append(key).append("|");
+            }
+        }
+        resourceRegex.deleteCharAt(resourceRegex.length() - 1);   //delete last extra pipe symbol
+        Log.d("Nitin", "resurce regex=" + String.valueOf(resourceRegex));
+
+        return resourceRegex.toString();
     }
 
     private void setAPICredentials() {
