edd624905c045becd056a7de3880f946dc297de4
Nitin <canvas.nv@gmail.com>
2018-11-14 01:51:24 +0530

    add to calendar fixed

--- app/src/main/java/com/Import/codetime/ContestDetailActivity.java
+++ app/src/main/java/com/Import/codetime/ContestDetailActivity.java
@@ -1,14 +1,9 @@
 package com.Import.codetime;
 
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -17,8 +12,7 @@ import com.Import.codetime.database.AppDatabase;
 import com.Import.codetime.database.ContestEntry;
 import com.Import.codetime.database.MyDiskExecutor;
 
-import java.util.Calendar;
-import java.util.TimeZone;
+import java.util.GregorianCalendar;
 
 public class ContestDetailActivity extends AppCompatActivity {
     public static final String ID_EXTRA_KEY = "idKey";
@@ -53,34 +47,39 @@ public class ContestDetailActivity extends AppCompatActivity {
     }
 
     public void addToCalender(View view) {
-        //bug: not working
-        Calendar startTime = Calendar.getInstance();
-        startTime.set(2018, 11, 12, 1, 19);
-        long startTimeMillis = startTime.getTimeInMillis();
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2018, 11, 13, 1, 20);
-        long endTimeMillis = endTime.getTimeInMillis();
+        //optimize: try to add a notification also, see documentation on androidDev.com
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra(CalendarContract.Events.TITLE, "time to CODE");
+        intent.putExtra(CalendarContract.Events.DESCRIPTION, "Check out this contest:\n" + contest.getName() + "\nClick on this link and register for it: " + contest.getURL());
 
-        ContentResolver contentResolver = getContentResolver();
-        ContentValues values = new ContentValues();
-        values.put(CalendarContract.Events.DTSTART, startTimeMillis);
-        values.put(CalendarContract.Events.DTEND, endTimeMillis);
-        values.put(CalendarContract.Events.TITLE, "time to CODE");
-        values.put(CalendarContract.Events.DESCRIPTION, "Check out this contest:\n" + contest.getName() + "\nClick on this link and register for it: " + contest.getURL());
-        values.put(CalendarContract.Events.CALENDAR_ID, 3);
-        values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getDisplayName());
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Uri uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values);
+        String start[] = contest.getStartDate().split("\n");
+        String startDate[] = start[0].split("-");
+        String startTime[] = start[1].split(":");
+        GregorianCalendar gregorianCalendar = new GregorianCalendar(
+                Integer.parseInt(startDate[0]),
+                Integer.parseInt(startDate[1]) - 1,   //month is zero based
+                Integer.parseInt(startDate[2]),
+                Integer.parseInt(startTime[0]),
+                Integer.parseInt(startTime[1].split(" ")[0])
+        );
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, gregorianCalendar.getTimeInMillis());
 
+        String end[] = contest.getEndDate().split("\n");
+        String endDate[] = end[0].split("-");
+        String endTime[] = end[1].split(":");
+        GregorianCalendar gregorianCalendar2 = new GregorianCalendar(
+                Integer.parseInt(endDate[0]),
+                Integer.parseInt(endDate[1]) - 1,
+                Integer.parseInt(endDate[2]),
+                Integer.parseInt(endTime[0]),
+                Integer.parseInt(endTime[1].split(" ")[0])
+        );
+        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, gregorianCalendar2.getTimeInMillis());
+
+        intent.putExtra(CalendarContract.Events.ACCESS_LEVEL, CalendarContract.Events.ACCESS_PRIVATE);
+        intent.putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
+        startActivity(intent);
     }
 
     public void shareContest(View view) {
