1ff19081c88da7aa7a804321acdeeb1143c88c8c
Nitin <canvas.nv@gmail.com>
2018-11-30 08:22:53 +0530

    extracted string resources and notification intent

--- app/src/main/java/com/Import/codetime/work/FetchDataWork.java
+++ app/src/main/java/com/Import/codetime/work/FetchDataWork.java
@@ -3,12 +3,15 @@ package com.Import.codetime.work;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 
+import com.Import.codetime.MainActivity;
 import com.Import.codetime.R;
 import com.Import.codetime.database.AppDatabase;
 
@@ -16,7 +19,7 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class FetchDataWork extends Worker {
-    Context mContext;
+    private Context mContext;
 
     public FetchDataWork(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -42,13 +45,17 @@ public class FetchDataWork extends Worker {
             notificationManager.createNotificationChannel(notificationChannel);
         }
 
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext, "channelId")
                 .setColor(ContextCompat.getColor(mContext, R.color.colorPrimary))
-                .setSmallIcon(R.drawable.ic_home)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle("CodeTime")
-                .setContentText("Database cleared")
+                .setContentText("Database cleared.Click to load fresh data")
                 .setDefaults(Notification.DEFAULT_VIBRATE)
-                .setTicker("app");
+                .setTicker("app")
+                .setContentIntent(pendingIntent);
 
         assert notificationManager != null;
         notificationManager.notify(154, builder.build());
